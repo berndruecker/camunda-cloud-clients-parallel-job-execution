cb81aa7b8caec4f2ac13c8b6a236f443c530fe5e
Simon Zengerling <simon.zengerling@camunda.com>
2021-07-18 13:42:25 +0200

    feat: some changes

--- go-worker/main.go
+++ go-worker/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/camunda-cloud/zeebe/clients/go/pkg/entities"
-	"github.com/camunda-cloud/zeebe/clients/go/pkg/pb"
 	"github.com/camunda-cloud/zeebe/clients/go/pkg/worker"
 	"github.com/camunda-cloud/zeebe/clients/go/pkg/zbc"
 	"log"
@@ -14,6 +13,7 @@ import (
 	"sync"
 	"time"
 )
+
 var PaymentUrl = "http://localhost:9090/"
 var startTimestamp time.Time
 var endTimestamp time.Time
@@ -33,15 +33,15 @@ func main() {
 	}
 	startInstances(zbClient)
 	startTimestamp = time.Now()
-	startNonBlockingWorker(zbClient)
-	//startBlockingWorker(zbClient)
+	startWorker(zbClient, handlerNonBlocking)
+	//startWorker(zbClient, handlerBlocking)
 }
 
 func startInstances(zbClient zbc.Client) {
 	ctx := context.Background()
-	log.Println("Start process instances...");
+	log.Println("Start process instances...")
 	var wg = sync.WaitGroup{}
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 10000; i++ {
 		wg.Add(1)
 		go func(index int) {
 			defer wg.Done()
@@ -53,12 +53,18 @@ func startInstances(zbClient zbc.Client) {
 	log.Println("...done")
 }
 
+func startWorker(zbClient zbc.Client, handler worker.JobHandler) {
+	w := zbClient.NewJobWorker().JobType("rest").Handler(handler).Open()
+	defer w.Close()
+	w.AwaitClose()
+}
+
 type nonBlocking struct {
 	Response *http.Response
 	Error    error
 }
 
-func PaymentRequest(nb chan nonBlocking) {
+func paymentRequest(nb chan nonBlocking) {
 	log.Println("Invoke REST call...")
 	resp, err := http.Get(PaymentUrl)
 	nb <- nonBlocking{
@@ -67,69 +73,63 @@ func PaymentRequest(nb chan nonBlocking) {
 	}
 }
 
-func HandleResponse(nb chan nonBlocking, client worker.JobClient, job entities.Job) {
-	compH := make(chan *pb.CompleteJobResponse)
-	wg := sync.WaitGroup{}
-	for get := range nb {
-		if get.Error != nil {
-			client.NewFailJobCommand().
-				JobKey(job.Key).
-				Retries(0).
-				ErrorMessage("Could not invoke REST API: " + get.Error.Error())
-		} else {
-			log.Println("...finished. Complete Job...")
-			wg.Add(1)
-			HandleComplete(client, job, &wg, compH)
+func handleResponse(nb chan nonBlocking, client worker.JobClient, job entities.Job) {
+	get := <-nb
+	if get.Error != nil {
+		ctx := context.Background()
+		_, err := client.NewFailJobCommand().
+			JobKey(job.Key).
+			Retries(1).
+			ErrorMessage("Could not invoke REST API: " + get.Error.Error()).
+			Send(ctx)
+		if err != nil {
+			log.Printf("Could not fail job: %s\n", err.Error())
+		}
+		log.Println("Could not invoke REST API " + get.Error.Error())
+	} else {
+		log.Println("...finished. Complete Job...")
+		go handleComplete(client, job)
+	}
+}
+
+func handleComplete(client worker.JobClient, job entities.Job) {
+	ctx := context.Background()
+	result, err := client.NewCompleteJobCommand().
+		JobKey(job.Key).
+		Send(ctx)
+	if err != nil {
+		log.Printf("Could not complete job: %s\n", err.Error())
+	}
+	if result != nil {
+		incCounter()
+	}
+}
+
+func handlerNonBlocking(client worker.JobClient, job entities.Job) {
+	nb := make(chan nonBlocking)
+	go paymentRequest(nb)
+	go handleResponse(nb, client, job)
+}
+
+func handlerBlocking(client worker.JobClient, job entities.Job) {
+	log.Println("Invoke REST call...")
+	resp, err := http.Get(PaymentUrl)
+	if err != nil {
+		log.Println("Error connecting to payment server")
+	}
+	if resp != nil {
+		log.Println("...finished. Complete Job...")
+		ctx := context.Background()
+		result, _ := client.NewCompleteJobCommand().
+			JobKey(job.Key).
+			Send(ctx)
+		if result != nil {
+			incCounter()
 		}
 	}
 }
 
-func HandleComplete(client worker.JobClient, job entities.Job, wg *sync.WaitGroup, compH chan *pb.CompleteJobResponse){
-	defer wg.Done()
-	ctx := context.Background()
-	result, _ := client.NewCompleteJobCommand().
-		JobKey(job.Key).
-		Send(ctx)
-	if result != nil {
-		incCounter()
-	}
-	compH <- result
-}
-
-func startNonBlockingWorker(zbClient zbc.Client) {
-	nb := make(chan nonBlocking)
-	zbClient.NewJobWorker().
-		JobType("rest").
-		Handler(func(client worker.JobClient, job entities.Job) {
-			go PaymentRequest(nb)
-			go HandleResponse(nb, client, job)
-		}).Open().AwaitClose()
-}
-
-func startBlockingWorker(zbClient zbc.Client) {
-	zbClient.NewJobWorker().
-		JobType("rest").
-		Handler(func(client worker.JobClient, job entities.Job) {
-			log.Println("Invoke REST call...")
-			resp, err := http.Get(PaymentUrl)
-			if err != nil {
-				log.Println("Error connecting to payment server")
-			}
-			if resp != nil {
-				log.Println("...finished. Complete Job...")
-				ctx := context.Background()
-				result, _ := client.NewCompleteJobCommand().
-					JobKey(job.Key).
-					Send(ctx)
-				if result != nil {
-					incCounter()
-				}
-			}
-		}).Open().AwaitClose()
-	log.Println("worker done")
-}
-
-func incCounter(){
+func incCounter() {
 	endTimestamp = time.Now()
 	count++
 	log.Printf("...completed (%s). ", getThroughputInfoFor(count))
